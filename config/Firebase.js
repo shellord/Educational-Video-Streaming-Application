1e7e9ca0f6eb91b126e9897ff70fc56ab71a0725
saheenshoukath <saheenshoukath@gmail.com>
2022-06-04 17:18:59 +0530

    Update Firebase.js

--- config/Firebase.js
+++ config/Firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBnDy_S-bFN7hNO3qvw3LYg-1unnoZ5PxM",
-  authDomain: "marvelapp-9d24b.firebaseapp.com",
-  projectId: "marvelapp-9d24b",
-  storageBucket: "marvelapp-9d24b.appspot.com",
-  messagingSenderId: "186055597146",
-  appId: "1:186055597146:web:fbd89572d04e746cfa0c63",
-  measurementId: "G-ZBP93X999Q"
+//API Config
 }
 
 const Firebase = firebase.initializeApp(firebaseConfig)
